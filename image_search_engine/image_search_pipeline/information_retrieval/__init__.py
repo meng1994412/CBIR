38ca985d089983002bd4cf99cd1c9e3959f6be8f
Chenyang Meng <sunnym412@gmail.com>
2018-10-28 01:39:27 -0500

    spatial verification editting

--- image_search_engine/image_search_pipeline/information_retrieval/__init__.py
+++ image_search_engine/image_search_pipeline/information_retrieval/__init__.py
@@ -2,3 +2,4 @@ from .vocabulary import Vocabulary
 from .bagofvisualwords import BagOfVisualWords
 from .searcher import Searcher
 from .dist import chi2_distance
+from .spatialverifier import SpatialVerifier
--- image_search_engine/image_search_pipeline/information_retrieval/spatialverifier.py
+++ image_search_engine/image_search_pipeline/information_retrieval/spatialverifier.py
@@ -1,7 +1,7 @@
 # import packages
 from .searchresult import SearchResult
 from sklearn.metrics import pairwise
-from imutils.feature import DescriptorExtractor_create
+from imutils.feature import DescriptorMatcher_create
 import numpy as np
 import datetime
 import h5py
@@ -15,6 +15,7 @@ class SpatialVerifier:
         # verification, and the reprojection threshold for computing inlier points
         self.idf = idf
         self.vocab = vocab
+        self.ratio = ratio
         self.minMatches = minMatches
         self.reprojThresh = reprojThresh
 
@@ -32,7 +33,7 @@ class SpatialVerifier:
         resultIdxs.sort()
 
         # loop over the starting and ending indexes into the features dataset for each image
-        for (i, (start, end)) in zip(resultIdxs, self.featuresDB["index"][resultsIdx, ...]):
+        for (i, (start, end)) in zip(resultIdxs, self.featuresDB["index"][resultIdxs, ...]):
             # grab the row from the features dataset and break the rows into
             # keypoints and feature vectors
             rows = self.featuresDB["features"][start: end]
@@ -53,7 +54,7 @@ class SpatialVerifier:
                 return searchResult
 
             # otherwise, sort the spatially verified results
-            results = sorted([(v, self.featuresDB["image_ids"][k], k) for (k, v) in reranked.items(), reverse = True]
+            results = sorted([(v, self.featuresDB["image_ids"][k], k) for (k, v) in reranked.items()], reverse=True)
 
             # loop over the initial search results
             for (score, imageID, imageIdx) in searchResult.results:
@@ -63,43 +64,43 @@ class SpatialVerifier:
                     results.append((score, imageID, imageIdx))
 
             # return the spatially verified and re-ranked results
-            return SearchResult(results[:numResults], (datetime.datetime.now() - startTime).to_second())
+            return SearchResult(results[:numResults], (datetime.datetime.now() - startTime).total_seconds())
 
-        def match(self, kpsA, featuresA, kpsB, featuresB):
-            # compute the raw matches and initialize the list of actual matches
-            # and the list of inlier indexes.
-            matcher = DescriptorExtractor_create("BruteForce")
-            rawMatches = matcher.knnMatch(featuresB, feturesA, 2)
-            matches = []
-            inlierIdxs = None
+    def match(self, kpsA, featuresA, kpsB, featuresB):
+        # compute the raw matches and initialize the list of actual matches
+        # and the list of inlier indexes.
+        matcher = DescriptorMatcher_create("BruteForce")
+        rawMatches = matcher.knnMatch(featuresB, featuresA, 2)
+        matches = []
+        inlierIdxs = None
 
-            # loop over the raw matches
-            for m in rawMatches:
-                # ensure the distance is within a certain ratio of each other
-                if len(m) == 2 and m[0].distance < m[1].distance * self.ratio:
-                    matches.append((m[0].trainIdx, m[0].queryIdx))
+        # loop over the raw matches
+        for m in rawMatches:
+            # ensure the distance is within a certain ratio of each other
+            if len(m) == 2 and m[0].distance < m[1].distance * self.ratio:
+                matches.append((m[0].trainIdx, m[0].queryIdx))
 
-            # check to see if there are enough matches to process
-            if len(matches) >= self.minMatches:
-                # construct the two sets of points
-                ptsA = np.float32([kpsA[i] for (i, _) in matches])
-                ptsB = np.float32([kpsB[j] for (_, j) in matches])
+        # check to see if there are enough matches to process
+        if len(matches) >= self.minMatches:
+            # construct the two sets of points
+            ptsA = np.float32([kpsA[i] for (i, _) in matches])
+            ptsB = np.float32([kpsB[j] for (_, j) in matches])
 
-                # compute the homography between two sets points and
-                # compute the ratio of matched points
-                (_, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, self.reprojThresh)
+            # compute the homography between two sets points and
+            # compute the ratio of matched points
+            (_, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, self.reprojThresh)
 
-                # grab the inlier points (i.e., where the status > 0)
-                # then quantize the features associated with these points
-                idxs = np.where(status.flatten() > 0)[0]
-                if idxs.shape[0] == 0:
-                    return None
-                inlierIdxs = pairwise.euclidean_distance(featuresB[idx], Y = self.vocab)
-                inlierIdxs = inlierIdxs.argmin(axis = 1)
+            # grab the inlier points (i.e., where the status > 0)
+            # then quantize the features associated with these points
+            idxs = np.where(status.flatten() > 0)[0]
+            if idxs.shape[0] == 0:
+                return None
+            inlierIdxs = pairwise.euclidean_distances(featuresB[idxs], Y=self.vocab)
+            inlierIdxs = inlierIdxs.argmin(axis = 1)
 
-            # return the list of inlier indexes
-            return inlierIdxs
+        # return the list of inlier indexes
+        return inlierIdxs
 
-        def finish(self):
-            # close the features database
-            self.featuresDB.close()
+    def finish(self):
+        # close the features database
+        self.featuresDB.close()
